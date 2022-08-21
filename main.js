9673c5462e16bfe7f2b96ff29986b3b63beee138
Meraki_DNA <dononichols@gmail.com>
2022-08-21 13:12:48 -0400

    -Complete JavaScript for NAV MENU : mobile

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert();
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector(".nav__items");
+
+
+
+//===========   NAV MENU :mobile ========
+
+//________ OPEN NAV MENU
+// add an Event Listener on the menuBtn
+menuBtn.addEventListener("click", () => {
+    menu.style.display = 'block'; // on click, show these events
+    menuBtn.style.display = 'none'; // on click, remove menu button
+    closeBtn.style.display = 'inline-block'; // and replace it with the closeBtn
+});
+
+//________ CLOSE NAV MENU
+// add an Event Listener on the menuBtn
+closeBtn.addEventListener("click", () => {
+    menu.style.display = 'none'; // on click, show these events
+    menuBtn.style.display = 'inline-block'; // on click, remove menu button
+    closeBtn.style.display = 'none'; // and replace it with the closeBtn
+});
